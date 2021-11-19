3bf2c6a178e40de63ae30f5355aa56dfec94debb
bao.ly <baobaobonbon@gmail.com>
2021-11-19 15:56:52 +0700

    first commit

---
+++ Dockerfile
@@ -0,0 +1,53 @@
+FROM ubuntu:20.04
+# Default environment variables. Please overwrite!
+
+ENV SAMBA_REALM="samba.dom"
+ENV SAMBA_PASSWORD="UnsecurePassword!"
+ENV SAMBA_HOST_IP="10.10.10.247"
+ENV SAMBA_DNS_FORWARDER="10.10.10.254"
+
+RUN apt-get update && \
+	apt-get install -y \
+       locales samba krb5-user krb5-config winbind smbclient libpam-winbind libnss-winbind \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+# Configure timezone and locale
+RUN echo "Asia/Ho_Chi_Minh" > /etc/timezone && \
+	dpkg-reconfigure -f noninteractive tzdata
+RUN locale-gen en_US.utf8 && \
+	dpkg-reconfigure locales
+	
+# Add startup scripts
+RUN mkdir -p /etc/my_init.d
+COPY samba_setup.sh samba_run.sh /etc/my_init.d/
+#COPY samba_run.sh /etc/my_init.d/
+#RUN chmod +x /etc/my_init.d/samba_setup.sh
+#RUN chown root:root /etc/my_init.d/samba_setup.sh
+#RUN chmod +x /etc/my_init.d/samba_run.sh
+#RUN chown root:root /etc/my_init.d/samba_run.sh
+
+VOLUME ["/var/lib/samba"]
+
+CMD ["/etc/my_init.d/samba_setup.sh"]
+
+# Clean up APT when done.
+#RUN apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+# Expose AD DC Ports
+EXPOSE 135/tcp
+EXPOSE 137/udp
+EXPOSE 138/udp
+EXPOSE 139/tcp
+EXPOSE 445/tcp
+EXPOSE 464/tcp
+EXPOSE 464/udp
+EXPOSE 389/tcp
+EXPOSE 389/udp
+EXPOSE 636/tcp
+EXPOSE 3268/tcp
+EXPOSE 3269/tcp
+EXPOSE 53/tcp
+EXPOSE 53/udp
+EXPOSE 88/tcp
+EXPOSE 88/udp
---
+++ samba_setup.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+set -e
+
+info () {
+    echo "[INFO] $@"
+}
+
+# Check if samba is setup
+[ -f /var/lib/samba/.setup ] && /etc/my_init.d/samba_run.sh
+
+# Require $SAMBA_REALM to be set
+: "${SAMBA_REALM:?SAMBA_REALM needs to be set}"
+
+# If $SAMBA_PASSWORD is not set, generate a password
+SAMBA_PASSWORD=${SAMBA_PASSWORD:-`(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c20; echo) 2>/dev/null`}
+info "Samba password set to: $SAMBA_PASSWORD"
+
+# Populate $SAMBA_OPTIONS
+SAMBA_OPTIONS=${SAMBA_OPTIONS:-}
+
+[ -n "$SAMBA_DOMAIN" ] \
+    && SAMBA_OPTIONS="$SAMBA_OPTIONS --domain=$SAMBA_DOMAIN" \
+    || SAMBA_OPTIONS="$SAMBA_OPTIONS --domain=${SAMBA_REALM%%.*}"
+
+[ -n "$SAMBA_HOST_IP" ] && SAMBA_OPTIONS="$SAMBA_OPTIONS --host-ip=$SAMBA_HOST_IP"
+
+# Fix nameserver
+echo "search ${SAMBA_REALM}\nnameserver 127.0.0.1" > /etc/resolv.conf
+
+# Provision domain
+rm -f /etc/samba/smb.conf
+rm -rf /var/lib/samba/*
+samba-tool domain provision \
+    --use-rfc2307 \
+    --realm=${SAMBA_REALM} \
+    --adminpass=${SAMBA_PASSWORD} \
+    --server-role=dc \
+    --dns-backend=SAMBA_INTERNAL \
+    $SAMBA_OPTIONS \
+    --option="bind interfaces only"=yes
+
+# Move smb.conf
+#mv /etc/samba/smb.conf /var/lib/samba/private/smb.conf
+
+# Update dns-forwarder if required
+[ -n "$SAMBA_DNS_FORWARDER" ] \
+    && sed -i "s/dns forwarder = .*/dns forwarder = $SAMBA_DNS_FORWARDER/" /etc/samba/smb.conf
+
+sed -i 's/\[sysvol\]/\[sysvol\]\n\tpreserve case = yes\n\tpublic = no\n\tcase sensitive = no\n\tvfs objects = dfs_samba4 acl_xattr\n\tbrowseable = no/' /etc/samba/smb.conf
+mv /etc/krb5.conf /etc/krb5.conf.original
+cp /var/lib/samba/private/krb5.conf /etc/
+echo nameserver 127.0.0.1 > /etc/resolv.conf
+echo domain $SAMBA_REALM >> /etc/resolv.conf
+cat /etc/resolv.conf
+sed -i "s/dns_lookup_realm = false/dns_lookup_realm = true/" /etc/krb5.conf
+
+# Mark samba as setup
+touch /var/lib/samba/.setup
+/etc/my_init.d/samba_run.sh
+
+# Setup only?
+[ -n "$SAMBA_SETUP_ONLY" ] && exit 127 || :
